7c51cd8947e25187b6ce058f0fa3a8f222c21b67
Jonas Laursen <jona8690@edu.eal.dk>
2017-02-07 18:23:27 +0100

    Nice console based ui :)

--- Threads02/Program.cs
+++ Threads02/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 using Threads02.Exceptions;
@@ -9,11 +11,14 @@ namespace Threads02 {
 
 		private static Random rnd = new Random();
 
-		private const int ServeTime = 1000;
-		private const int GrowTime = 500;
-		private const int QueEmptyWait = 2500;
+		private int ServeTime = rnd.Next(1000, 1500);
+		private int GrowTime = rnd.Next(500, 750);
+		private int QueEmptyWait = 1000;
 
 		private bool ShopsOpen = true;
+
+		private Dictionary<int, int> Clerks = new Dictionary<int, int>();
+
 		static void Main(string[] args) {
 			Program a = new Program();
 			a.Run();
@@ -24,48 +29,82 @@ namespace Threads02 {
 			Thread Clerk0 = new Thread(ServeCustomer);
 			Thread Clerk1 = new Thread(ServeCustomer);
 
-			Console.WriteLine("Click enter to close up shop...");
+			Thread UI = new Thread(ConsoleUI);
 
+			UI.Start();
+			
 			Customers.Start();
 			Clerk0.Start();
 			Clerk1.Start();
 
 			Console.ReadLine();
 			ShopsOpen = false;
-			Console.WriteLine("Closing shop, no new numbers...");
-
+			
 			Customers.Join();
 			Clerk0.Join();
 			Clerk1.Join();
+
+			UI.Abort();
 
 			Console.WriteLine("No more customers to be served...");
 			Console.ReadKey();
 		}
 		
-		private void ConsoleUI() {
+		public void ConsoleUI() {
+			while(true) {
+				Console.Clear();
+				Console.WriteLine("Click enter to close up shop...\n");
 
+				Console.WriteLine("Currently in Que: " + que.InQue);
+				Console.WriteLine("Next Number Available: " + que.NextNumber);
+
+				foreach(KeyValuePair<int, int> Clerk in Clerks) {
+					Console.WriteLine("Clerk "+ Clerk.Key +" is currently serving " + Clerk.Value);
+				}
+
+				if(!ShopsOpen) {
+					Console.WriteLine("\nClosing shop, no new numbers...");
+				}
+
+				Thread.Sleep(125);
+			}
 		}
 
 		private void NewCustomer() {
 			while (ShopsOpen) {
 				int MyNumber = que.PullNumber();
-				Console.WriteLine("The next number to be picked: " + MyNumber);
+				//Console.WriteLine("The next number to be picked: " + MyNumber);
 
 				Thread.Sleep(GrowTime);
 			}
 		}
 
 		private void ServeCustomer() {
+			int ClerkID = RegisterClerk();
 			while (ShopsOpen || que.InQue > 0) {
 				try {
 					int Serving = que.serveNext();
-					Console.WriteLine("Now handling: " + Serving);
+					Clerks[ClerkID] = Serving;
+					//Console.WriteLine("Now handling: " + Serving);
 					Thread.Sleep(ServeTime);
 				} catch (QueIsEmptyException) {
-					Console.WriteLine("Que is empty...");
+					Clerks[ClerkID] = 0;
+					//Console.WriteLine("Que is empty...");
 					Thread.Sleep(QueEmptyWait);
 				}
 			}
+		}
+
+		private int RegisterClerk() {
+			int newID;
+			if (Clerks.Count == 0) {
+				newID = 0;
+			} else {
+				newID = Clerks.Keys.Last() + 1;
+			}
+						
+			Clerks.Add(newID, 0);
+			return newID;
 		}
 	}
 }
